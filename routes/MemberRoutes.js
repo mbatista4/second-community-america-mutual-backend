58df3fa8ed58bc6028a48c88d521c4094bef3a70
dpr <rbourque64@gmail.com>
2021-11-03 13:54:21 -0400

    removed loggout

--- routes/MemberRoutes.js
+++ routes/MemberRoutes.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcrypt");
 const router = require("express").Router();
 const Member = require('../models/Member');
 
@@ -21,7 +22,6 @@ router.post('register_member', async (req,res)=>{
 	} = req.body;
 
 	
-	// check if ssn is already in use or bad length
 	const ssnInUse = await Member.findOne({socialSecurityNumber});
 
 	if(ssnInUse)
@@ -34,7 +34,6 @@ router.post('register_member', async (req,res)=>{
 		return res.status(409).json({msg: "SSN invalid!"});
 	}
 
-		//		if not check if userid is in use
 	const idInUse = await Member.findOne({userId});
 
 	if(idInUse){
@@ -42,11 +41,11 @@ router.post('register_member', async (req,res)=>{
 		return res.status(409).json({msg: "Account id already exists!"});
 	}
 
-		//			if not check if pwrd is not compliant
+	
 	if(password.length < 8){
 		return res.status(409).json({msg: "Password not compliant!"});
 	}
-		//				else add to db
+		
 
 	const newmember = new Member({
 
@@ -67,65 +66,25 @@ router.post('register_member', async (req,res)=>{
 
 router.post('/login_member', async (req,res)=>{
 
-
 	let {
-
 		userId,
 		password,
-
 	} = req.body;
-
 
     const existingAccount = await Member.findOne({userId});
 
 
+	let isPassword = await bcryptjs.compare(existingAccount.password,password);
 
-
-
-    if(!existingAccount || existingAccount.password.localeCompare(password) != 0) {
+    if(!existingAccount || !isPassword) {
        return res.status(409).json({msg: "account does not exists or password is inccorrect!"});
     }
 
 
-
-    existingAccount.isLoggdIn = true;
-
  const token = jwt.sign({
         user: existingAccount._id,
-	},process.env.SECRET,{expiresIn: '8h'});
-
-
-
-	try {
-        let response =  await existingAccount.save();
-
-        console.log(response._id);
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({msg: "An error occured when trying to login. please try again in a few minutes",error});
-    }
-
+	},process.env.SECRET,{expiresIn: '1h'});
 
 	return res.status(200).json(token);
 
-
-});
-
-
-
-
-
-
-router.post('logout_member/:id', async (req,res)=>{
-
-    let {id} = req.params;
-
-    const Account = await Member.findOne({_id: id});
-
-	Account.isClockedIn = false;
-
-	let response =  await Account.save();
- 
-	 console.log("logged out");
-	 return res.status(200).json({response});
 });

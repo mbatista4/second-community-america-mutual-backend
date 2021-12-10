39cda6ddfc960cef3f906ba3cac7f09201492b69
mbatista4 <manuelbatista04@gmail.com>
2021-12-10 11:42:27 -0500

    exposed admin routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const bankAccountRoutes = require('./BankAccountRoutes');
 const memberRoutes = require('./MemberRoutes');
 const tellerRoutes = require('./TellerRoutes');
+const adminRoutes = require('./AdminRoutes');
+const adminFunctions = require('./AdminfunctionsRoutes');
+adminRoutes.use(adminFunctions)
 
 module.exports = {
     bankAccountRoutes,
     memberRoutes,
-    tellerRoutes
+    tellerRoutes,
+    adminRoutes
 }
